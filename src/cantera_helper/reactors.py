b29dbf1b806384557486842730dc72560d78aa7b
avcopan <avcopan@gmail.com>
2025-10-16 16:14:49 -0400

    Replaces old O2 sweep example with minor refactoring

--- src/cantera_helper/reactors.py
+++ src/cantera_helper/reactors.py
@@ -1,49 +1,46 @@
 """Cantera reactors."""
 
 from collections.abc import Mapping
-from numbers import Number
 
-Solution = object  # cantera.Solution
-ReactorNet = object  # cantera.ReactorNet
+import cantera as ct
+from cantera import ReactorNet, Solution  # pyright: ignore[reportAttributeAccessIssue]
 
 
 def jsr(
     model: Solution,
-    temp: Number,
-    pres: Number,
-    tau: Number,
-    vol: Number,
-    conc: Mapping[str, Number],
+    T: float,
+    P: float,
+    residence_time: float,
+    volume: float,
+    concentrations: Mapping[str, float],
 ) -> ReactorNet:
     """Run a jet-stirred reactor simulation.
 
     :param model: Chemical kinetics model
-    :param temp: Temperature
-    :param pres: Pressure
-    :param tau: Residence time
-    :param conc: Starting concentrations
+    :param T: Temperature
+    :param P: Pressure
+    :param residence_time: Residence time
+    :param concentrations: Starting concentrations
     :return: Solved simulation reactor network
     """
-
-    import cantera as ct
-
     # Use concentrations from the previous iteration to speed up convergence
-    model.TPX = temp, pres, conc
+    model.TPX = T, P * ct.one_atm, concentrations  # pyright: ignore[reportAttributeAccessIssue]
 
     # Set up JSR: inlet -> flow control -> reactor -> pressure control -> exhaust
-    reactor = ct.IdealGasReactor(model, energy="off", volume=vol)
-    exhaust = ct.Reservoir(model)
-    inlet = ct.Reservoir(model)
-    ct.PressureController(
+    volume_m3 = volume * (1e-2) ** 3
+    reactor = ct.IdealGasReactor(model, energy="off", volume=volume_m3)  # pyright: ignore[reportAttributeAccessIssue]
+    exhaust = ct.Reservoir(model)  # pyright: ignore[reportAttributeAccessIssue]
+    inlet = ct.Reservoir(model)  # pyright: ignore[reportAttributeAccessIssue]
+    ct.PressureController(  # pyright: ignore[reportAttributeAccessIssue]
         upstream=reactor,
         downstream=exhaust,
         K=1e-3,
-        primary=ct.MassFlowController(
+        primary=ct.MassFlowController(  # pyright: ignore[reportAttributeAccessIssue]
             upstream=inlet,
             downstream=reactor,
-            mdot=reactor.mass / tau,
+            mdot=reactor.mass / residence_time,
         ),
     )
-    reactor_net = ct.ReactorNet([reactor])
+    reactor_net = ct.ReactorNet([reactor])  # pyright: ignore[reportAttributeAccessIssue]
     reactor_net.advance_to_steady_state(max_steps=100000)
     return reactor
